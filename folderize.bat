e6ed623ab07598d72e15992230a3bb58db53a1bc
Matt R <mattrdk2@gmail.com>
2020-03-05 16:53:21 -0800

    Update this again with the final path

--- folderize.bat
+++ folderize.bat
@@ -1,6 +1,6 @@
 @echo off
 for %%i in (\Movies\*) do (
  if not "%%~ni" == "organize" (
-  md "%%~ni" && move "%%~i" "%%~ni" && move "%%~ni" E:\Movies\
+  md "%%~ni" && move "%%~i" "%%~ni" && move "%%~ni" E:\Server\Movies\
  )
 )
